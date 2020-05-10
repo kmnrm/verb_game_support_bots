5f9a652bfdf276bf7db37af6853927377d75859c
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-05-11 00:07:38 +0800

    Simplify code

--- shape_intents.py
+++ shape_intents.py
@@ -15,18 +15,18 @@ def shape_intents_from_file(training_questions_json_file_name):
         for intent_name, qna in training_questions.items():
             questions = qna["questions"]
             answers = [qna["answer"]]
-            training_phrases = list(
+            training_phrases = [
                 {
                     "parts": [{"text": training_phrase}]
                 }
                 for training_phrase in questions
-            )
-            messages = list(
+            ]
+            messages = [
                 {
                     "text": {"text": [message]}
                 }
                 for message in answers
-            )
+            ]
             intent = {"display_name": intent_name,
                       "messages": messages,
                       "training_phrases": training_phrases
