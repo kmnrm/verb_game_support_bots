2fdc1f7362dabebdf7d2fdd34dc5c8522a967ac2
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-05-11 00:02:57 +0800

    Clarify code

--- shape_intents.py
+++ shape_intents.py
@@ -2,14 +2,14 @@ import json
 import logging
 
 
-def open_json_file(json_file_name):
+def read_json_file(json_file_name):
     with open(json_file_name, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def shape_intents_from_file(training_questions_json_file_name):
     try:
-        training_questions = open_json_file(training_questions_json_file_name)
+        training_questions = read_json_file(training_questions_json_file_name)
         logging.debug('Training questions received')
         intents = []
         for intent_name, qna in training_questions.items():
